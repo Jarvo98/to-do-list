f800baa30c01abf47f4935d0cd952b195852aec6
Jesus <jesus@ensolvers.com>
2020-02-07 16:28:02 -0300

    Creating the FolderLink component and giving it styles.

--- src/components/MainPage/Sidebar/FolderLink/FolderLink.js
+++ src/components/MainPage/Sidebar/FolderLink/FolderLink.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./FolderLink.css";
 
 const FolderLink = () => {
-  return <p>Hola</p>;
+  return (
+    <a href="/" className="buttonContainer">
+      <span href="/">hola</span>
+    </a>
+  );
 };
 
 export default FolderLink;
--- src/components/MainPage/Sidebar/Sidebar.js
+++ src/components/MainPage/Sidebar/Sidebar.js
@@ -3,20 +3,14 @@ import "./Sidebar.css";
 import FolderLink from "./FolderLink/FolderLink";
 
 class Sidebar extends React.Component {
-  folders = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  folders = ["", "", "", "", "", "", "", "", "", ""];
 
   render() {
     return (
       <div className="shadowBox">
-        <ul>
-          {this.folders.map(item => {
-            return (
-              <li>
-                <FolderLink />
-              </li>
-            );
-          })}
-        </ul>
+        {this.folders.map(item => {
+          return <FolderLink />;
+        })}
       </div>
     );
   }
