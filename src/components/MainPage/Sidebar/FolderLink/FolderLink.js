3221a22b8c88286afc9b3f5c60f4a0a679af3225
Jesus <jesus@ensolvers.com>
2020-02-07 09:08:07 -0300

    Continued to work with the sidebar.

---
+++ src/components/MainPage/Sidebar/FolderLink/FolderLink.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FolderLink.css";
+
+const FolderLink = () => {
+  return <p>Hola</p>;
+};
+
+export default FolderLink;
--- src/components/MainPage/Sidebar/Sidebar.js
+++ src/components/MainPage/Sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
+import FolderLink from "./FolderLink/FolderLink";
 
 class Sidebar extends React.Component {
   folders = ["Item 1", "Item 2", "Item 3", "Item 4"];
@@ -9,7 +10,11 @@ class Sidebar extends React.Component {
       <div className="shadowBox">
         <ul>
           {this.folders.map(item => {
-            return <li>{item}</li>;
+            return (
+              <li>
+                <FolderLink />
+              </li>
+            );
           })}
         </ul>
       </div>
