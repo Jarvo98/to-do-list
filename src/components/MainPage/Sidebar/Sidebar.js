2905f8c21fb6a1f27ba7af85ae917f176a31384a
Jesus <jesus@ensolvers.com>
2020-02-11 17:13:05 -0300

    Forgot to add the modification of the Sidebar component wich I change to a functional component due to it not having more logic inside it to qualify as Class component.

--- src/components/MainPage/Sidebar/Sidebar.js
+++ src/components/MainPage/Sidebar/Sidebar.js
@@ -2,22 +2,16 @@ import React from "react";
 import "./Sidebar.css";
 import FolderLink from "./FolderLink/FolderLink";
 
-class Sidebar extends React.Component {
-  render() {
-    return (
-      <div className="shadowBox">
-        {this.props.folders.map((item, index) => {
-          return (
-            <FolderLink
-              folderInfo={item}
-              index={index}
-              onClick={this.props.onClick}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Sidebar = props => {
+  return (
+    <div className="shadowBox">
+      {props.folders.map((item, index) => {
+        return (
+          <FolderLink folderInfo={item} index={index} onClick={props.onClick} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Sidebar;
